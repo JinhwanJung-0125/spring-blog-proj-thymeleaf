536c1aaa6f9e8f15b9d6a0fa70be231b64cfc461
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-15 19:13:25 +0900

    Feat : findAllByArticleId() 메소드 추가 및 Query 구현

--- src/main/java/com/springblogproj/repository/CommentRepository.java
+++ src/main/java/com/springblogproj/repository/CommentRepository.java
@@ -1,10 +1,15 @@
 package com.springblogproj.repository;
 
 import com.springblogproj.domain.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @Query(value = "SELECT * FROM comment WHERE article_id = :articleId ORDER BY id", nativeQuery = true)
+    List<Comment> findAllByArticleId(@Param(value = "articleId") Long articleId);
 }
