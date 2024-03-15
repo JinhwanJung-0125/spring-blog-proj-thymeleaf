2af853efbfa3d18fc539aa6e677190edff5bc09c
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-15 19:30:24 +0900

    Feat : findCommentByCommentId() 메소드 추가 및 Query 구현

--- src/main/java/com/springblogproj/repository/CommentRepository.java
+++ src/main/java/com/springblogproj/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.springblogproj.repository;
 
 import com.springblogproj.domain.Comment;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query(value = "SELECT * FROM comment WHERE id = :commentId AND article_id = :articleId ORDER BY id", nativeQuery = true)
+    Optional<Comment> findCommentByCommentId(@Param(value = "articleId") Long articleId,
+        @Param(value = "commentId") Long commentId);
 
     @Query(value = "SELECT * FROM comment WHERE article_id = :articleId ORDER BY id", nativeQuery = true)
     List<Comment> findAllByArticleId(@Param(value = "articleId") Long articleId);
